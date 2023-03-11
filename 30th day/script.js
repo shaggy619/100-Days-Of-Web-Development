c3271a0dab00b95d1058294a973e642f2e7b32e0
shaggy <someaim1@gmail.com>
2023-03-11 09:53:23 +0545

    Added New Files

---
+++ 30th day/script.js
@@ -0,0 +1,17 @@
+alert("Hi, I'm Samyam");
+//creating a variable requires "let" keyword followed by variable name and value.
+let userName = "Samyam";
+//another way to create variable
+let demoName;
+demoName = "Dummy Text";
+//for numeric value we don't use "" sing
+let age = 21;
+alert(userName);
+alert(age);
+//We can create array using [] bracket where value are seperated by comma ",";
+let hobbies = ["Music Producing", "Travelling", "Coding"];
+alert(hobbies[0]);
+/*for creating an object we need to use {} bracket which contains two value, the first is title
+and the second is value for that title. The value can be added by putting ":" sign after the
+title name. */
+let job = { title: "Web Developer", place: "Kathmandu", salary: "60K" };
